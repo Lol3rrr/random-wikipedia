e884a1442061b7bc49d0aff902d5a1d453a0bc16
lol3rrr <s.loler03@gmail.com>
2020-08-28 06:41:50 +0200

    Fixed a small problem

--- login/generateLoginURL.go
+++ login/generateLoginURL.go
@@ -1,10 +1,18 @@
 package login
 
-import "net/url"
+import (
+	"net/url"
+	"path"
+)
 
-func generateLoginURL(baseURL, email, password string) string {
+func generateLoginURL(rawBaseURL, email, password string) string {
 	escapedEmail := url.QueryEscape(email)
 	escapedPassword := url.QueryEscape(password)
 
-	return baseURL + "/login/confirm?email=" + escapedEmail + "&password=" + escapedPassword
+	baseURL, _ := url.Parse(rawBaseURL)
+	baseURL.Path = path.Join(baseURL.Path, "/login/confirm")
+	baseURL.Query().Add("email", escapedEmail)
+	baseURL.Query().Add("password", escapedPassword)
+
+	return baseURL.String()
 }
