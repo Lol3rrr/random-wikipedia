6e8d6ba7a596561c46e913f16f8f9f22bacb9015
lol3rrr <s.loler03@gmail.com>
2020-08-29 00:56:17 +0200

    Updated development script

--- dev_setup.bash
+++ dev_setup.bash
@@ -19,7 +19,7 @@ VAULT_ROOT=$(docker logs $VAULT_ID 2>&1 | awk -F': ' '/Root Token: /{print $2}')
 VAULT_ADDR='http://0.0.0.0:8200' vault login $VAULT_ROOT
 VAULT_ADDR='http://0.0.0.0:8200' vault secrets enable database
 VAULT_ADDR='http://0.0.0.0:8200' vault write database/config/postgres plugin_name="postgresql-database-plugin" connection_url="postgresql://{{username}}:{{password}}@localhost:5432/devWikipedia?sslmode=disable" allowed_roles="random-wikipedia" username="devAdmin" password="devPassword"
-VAULT_ADDR='http://0.0.0.0:8200' vault write database/roles/random-wikipedia db_name="postgres" creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT SELECT ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" default_ttl="1h"
+VAULT_ADDR='http://0.0.0.0:8200' vault write database/roles/random-wikipedia db_name="postgres" creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT ALL ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" default_ttl="1h"
 VAULT_ADDR='http://0.0.0.0:8200' vault auth enable approle
 VAULT_ADDR='http://0.0.0.0:8200' vault policy write random-wikipedia ./policy.hcl
 VAULT_ADDR='http://0.0.0.0:8200' vault write auth/approle/role/random-wikipedia token_ttl="20m" policies="random-wikipedia"
