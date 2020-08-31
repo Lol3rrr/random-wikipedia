a9247010c44131326c5ea12093b8fd8366e4bd7f
lol3rrr <s.loler03@gmail.com>
2020-08-31 17:05:23 +0200

    Added another improvement

--- api/authMiddleware.go
+++ api/authMiddleware.go
@@ -1,15 +1,30 @@
 package api
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/gofiber/fiber"
 	"github.com/sirupsen/logrus"
 )
 
+func getSessionID(ctx *fiber.Ctx) (string, error) {
+	sessionCookie := ctx.Cookies("SessionID")
+	if len(sessionCookie) > 0 {
+		return sessionCookie, nil
+	}
+
+	sessionHeader := ctx.Get("x-session")
+	if len(sessionCookie) > 0 {
+		return sessionHeader, nil
+	}
+
+	return "", errors.New("Could not find SessionID")
+}
+
 func (a *api) Auth(ctx *fiber.Ctx) {
-	sessionID := ctx.Cookies("SessionID")
-	if len(sessionID) <= 0 {
+	sessionID, err := getSessionID(ctx)
+	if err != nil {
 		ctx.SendStatus(http.StatusUnauthorized)
 		return
 	}
