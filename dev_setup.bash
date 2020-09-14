c5da8bf40d32a44730e9a4fd3fa155206a16dd9d
lol3rrr <s.loler03@gmail.com>
2020-09-14 06:45:52 +0200

    Updated the dev-env setup to make things a bit more obvious

--- dev_setup.bash
+++ dev_setup.bash
@@ -4,15 +4,11 @@ POSTGRES_ID=$(docker run -d -e POSTGRES_PASSWORD=devPassword -e POSTGRES_USER=de
 echo "Postgres: $POSTGRES_ID";
 
 sleep 3s;
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE TABLE IF NOT EXISTS Users (ID TEXT NOT NULL PRIMARY KEY, SessionID TEXT NOT NULL, Email TEXT NOT NULL);';
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE TABLE IF NOT EXISTS Passwords (ID TEXT NOT NULL PRIMARY KEY, Password TEXT NOT NULL, Expiration INTEGER);';
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE TABLE IF NOT EXISTS Notifications (ID TEXT NOT NULL PRIMARY KEY, Subscription TEXT NOT NULL);';
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE TABLE IF NOT EXISTS Settings (ID TEXT NOT NULL PRIMARY KEY, Notifytime INTEGER);';
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE TABLE IF NOT EXISTS Userlists (ID TEXT NOT NULL, ListID INTEGER);';
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE UNIQUE INDEX idx_Userlists_ID_ListID on Userlists (ID, ListID);';
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE TABLE IF NOT EXISTS Lists (ListID INTEGER PRIMARY KEY, Title TEXT NOT NULL);';
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE TABLE IF NOT EXISTS Favorites (ID TEXT NOT NULL PRIMARY KEY, ArticleID INTEGER);';
-docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -c 'CREATE TABLE IF NOT EXISTS FavArticles (ArticleID INTEGER PRIMARY KEY, Title TEXT NOT NULL, URL TEXT NOT NULL);';
+docker exec -it $POSTGRES_ID ls
+docker cp ./schema.sql $POSTGRES_ID:/schema.sql
+docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -f schema.sql
+docker cp ./index.sql $POSTGRES_ID:/index.sql
+docker exec -it $POSTGRES_ID psql -d devWikipedia -U devAdmin -f index.sql
 
 VAULT_ID=$(docker run --cap-add=IPC_LOCK -d --network host -e 'VAULT_LOCAL_CONFIG={"backend": {"file": {"path": "/vault/file"}}}' vault server -dev);
 echo "Vault: $VAULT_ID";
@@ -22,7 +18,7 @@ VAULT_ROOT=$(docker logs $VAULT_ID 2>&1 | awk -F': ' '/Root Token: /{print $2}')
 VAULT_ADDR='http://0.0.0.0:8200' vault login $VAULT_ROOT
 VAULT_ADDR='http://0.0.0.0:8200' vault secrets enable database
 VAULT_ADDR='http://0.0.0.0:8200' vault write database/config/postgres plugin_name="postgresql-database-plugin" connection_url="postgresql://{{username}}:{{password}}@localhost:5432/devWikipedia?sslmode=disable" allowed_roles="random-wikipedia" username="devAdmin" password="devPassword"
-VAULT_ADDR='http://0.0.0.0:8200' vault write database/roles/random-wikipedia db_name="postgres" creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT ALL ON ALL TABLES IN SCHEMA public TO \"{{name}}\";" revocation_statements="DROP OWNED BY \"{{name}}\"; DROP user \"{{name}}\";" default_ttl="1h"
+VAULT_ADDR='http://0.0.0.0:8200' vault write database/roles/random-wikipedia db_name="postgres" creation_statements="CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}'; GRANT ALL ON ALL TABLES IN SCHEMA randomWikipedia TO \"{{name}}\";" revocation_statements="DROP OWNED BY \"{{name}}\"; DROP user \"{{name}}\";" default_ttl="1h"
 VAULT_ADDR='http://0.0.0.0:8200' vault auth enable approle
 VAULT_ADDR='http://0.0.0.0:8200' vault policy write random-wikipedia ./policy.hcl
 VAULT_ADDR='http://0.0.0.0:8200' vault write auth/approle/role/random-wikipedia token_ttl="20m" policies="random-wikipedia"
